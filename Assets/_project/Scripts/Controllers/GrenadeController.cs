3866b21f5e7a2afa16bee4e77db2d2e0a3e12160
DrollestHook <nozdrinns@gmail.com>
2021-05-25 12:09:17 +0300

    fix Grenade aim system

--- Assets/_project/Scripts/Controllers/GrenadeController.cs
+++ Assets/_project/Scripts/Controllers/GrenadeController.cs
@@ -8,9 +8,11 @@ namespace Project.Controllers {
         [SerializeField] private Grenade _grenadePrefab = default;
         [SerializeField] private Transform _weaponHolder = default;
         [SerializeField] private float _maxThrowForce = default;
-        [SerializeField] private Vector3 _defaultDirection = new Vector3(0,1,1);
+        [Header("LineRenderer Setup")]
         [SerializeField] private LineRenderer _lineRenderer = default;
+        [SerializeField] private float _maxLineRendererLength = 3f;
         [SerializeField] private int _iterations = 20;
+        [SerializeField] private LayerMask _grenadeCollisionsMask;
 
         private Unit unit;
         private Grenade _currentGrenade;
@@ -18,6 +20,7 @@ namespace Project.Controllers {
         private void Awake() {
             unit = GetComponent<Unit>();
             _lineRenderer.positionCount = _iterations;
+            _lineRenderer.enabled = false;
         }
         
         public void EquippGrenade() {
@@ -33,16 +36,31 @@ namespace Project.Controllers {
         }
 
         public void VisualizeAim(float force) {
-            for (int i = 0; i < _iterations; i++) {
-                Vector3 position = CalculatePosInTime((transform.forward + transform.up) * force * _maxThrowForce, i /(float)_iterations);
-                _lineRenderer.SetPosition(i,position);
+            Vector3 startVelocity = (transform.forward + transform.up) * force * _maxThrowForce;
+            DrawLineRenderer(startVelocity, GetAmountOfIterations(startVelocity) + 1);
+        }
+
+        private int GetAmountOfIterations(Vector3 velocity) {
+            int i = 0;
+            for (; i < _iterations; i++) {
+                Vector3 position = CalculatePosInTime(velocity, i*_maxLineRendererLength /(float)_iterations);
+                Vector3 nexPosition = CalculatePosInTime(velocity, (i + 1) * _maxLineRendererLength / (float) _iterations);
+                if (Physics.Raycast(position, nexPosition - position, Vector3.Distance(position, nexPosition), _grenadeCollisionsMask)) {
+                    break;
+                }
+            }
+            return i;
+        }
+
+        private void DrawLineRenderer(Vector3 velocity, int iterations) {
+            _lineRenderer.positionCount = iterations;
+            for (int i = 0; i < iterations; i++) {
+                Vector3 position = CalculatePosInTime(velocity, i * _maxLineRendererLength / (float) _iterations);
+                _lineRenderer.SetPosition(i, position);
             }
         }
 
         private Vector3 CalculatePosInTime(Vector3 startVelocity, float time) {
-            Vector3 Vxz = startVelocity;
-            Vxz.y = 0f;
-
             Vector3 resultPos = _weaponHolder.position + startVelocity * time;
             float sY = (-0.5f * Mathf.Abs(Physics.gravity.y) * time * time) + startVelocity.y * time + _weaponHolder.position.y;
             resultPos.y = sY;
